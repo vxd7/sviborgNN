62fbe6d22f6680011b5d4f9e247a0b7bfa14249e
_VXD7 <youmukonpaku@excite.co.jp>
2016-11-10 12:50:45 +0300

    WIP: Layer header file

--- src/ConvLayer.h
+++ src/ConvLayer.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <iostream>
+#include <string>
 #include <vector>
 
-// #include "ConvNeuron.h"
+#include "ConvNeuron.h"
 // #include "Log.h"
 
 class ConvLayer {
@@ -14,16 +14,18 @@ private:
 	int inputMapWidth;
 
 	std::vector <ConvNeuron> neurons;
+
 public:
-	ConvLayer();
+	ConvLayer(const int neuronCount, const int mapHeight, const int mapWidth);
 	~ConvLayer();
-	
-	void initLayer(const int neuronCount, const int mapHeight, const int mapWidth);
 
 	void computeFeatureMaps(std::vector<std::vector<double>> inputMap);
 
-	void writeNeuronsToFiles(...);
-	void readNeuronsFromFile(...);
+	void writeMapsToFiles(std::string fileNamePrefix);
+	void readMapsFromFiles(std::string fileNamePrefix);
 
+	void writeSingleMap(std::string fileNamePrefix, int neuronNumber);
+	void readSingleMap(std::string fileNamePrefix, int neuronNumber);
 
+	void updateMap(std::vector<std::vector<double>> &updMap, int neuronNumber);
 };
