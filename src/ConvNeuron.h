2bc1c02571c584af7dc437d04a3eb3c2112c297d
_VXD7 <youmukonpaku@excite.co.jp>
2016-11-12 15:49:38 +0300

    Additional bugfixes

--- src/ConvNeuron.h
+++ src/ConvNeuron.h
@@ -5,7 +5,7 @@
 class ConvNeuron {
 private:
     std::vector<std::vector<double>> convCore;
-    std::vector<std::vector<double>> featureMap;
+    std::vector<std::vector<double>> outputFeatureMap; //Out of the neuron
 
     double aWeight;
 
@@ -16,14 +16,14 @@ private:
 
     void randomWeights();
 
-    double summate(const std::vector <std::vector<double>> &map, int ipos, int jpos); // may be this is shit, but i wanted to optimise the code;
+    double summate(const std::vector <std::vector<double>> &inputMap, int ipos, int jpos); // may be this is shit, but i wanted to optimise the code;
     double tFunc(double x);
 
 public:
     ConvNeuron(int height, int length, bool isRand = false); // random weights if isRand = true, else pushback 0.0;
     ~ConvNeuron();
 
-    void processMap(std::vector <std::vector<double>> &map); // creating an output feautureMap;
+    void processMap(std::vector <std::vector<double>> &inputMap); // creating an output feautureMap;
 
     void initNeuron(); // empty for now;
 };
