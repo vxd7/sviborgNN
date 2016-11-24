cad2c737a0283e4f56f6e4ad4ba2bb00ed136fd1
_VXD7 <youmukonpaku@excite.co.jp>
2016-11-24 21:31:10 +0300

    processInput function

--- src/ConvNetwork.cpp
+++ src/ConvNetwork.cpp
@@ -1,6 +1,10 @@
 #include <stdexcept>
 #include <utility>
+#include <string>
+
 #include "ConvNetwork.h"
+#include "NetworkException.h"
+#include "ImageBMP.h"
 
 ConvNetwork::ConvNetwork(const std::vector<int> neuronsInLayers, const std::vector<std::pair<int, int>> &convCoresDim) {
 
@@ -38,6 +42,45 @@ ConvNetwork::ConvNetwork(const std::vector<int> neuronsInLayers, const std::vect
 	}
 }
 
-void ConvNetwork::processOutput(std::vector <double> &outputMap) {
-	
+void ConvNetwork::processInput(std::vector <double> &outputMap) {
+	/* Image is loaded and network is built by now */
+
+	networkLayers[0].computeFeatureMap(inputMap);
+
+	for(size_t i = 1; i < numLayers; ++i) {
+		std::vector<std::vector<std::vector<double>>> layerOutput;
+
+		networkLayers[i].getAllFeatureMaps(layerOutput);
+
+		if(networkLayers[i].type == CONV) {
+			networkLayers[i].computeFeatureMaps(layerOutput);
+		}
+
+		if(networkLayers[i].type == SUBSAMPLE) {
+			networkLayers[i].subsampleFeatureMaps(layerOutput);
+		}
+	}
+
+	std::vector<std::vector<std::vector<double>>> lastLayerOutput;
+	int lastLayer = networkLayers.size() - 1;
+
+	networkLayers[lastLayer].getAllFeatureMaps(lastLayerOutput);
+
+	/* Check result feature maps for validity */
+	bool validNetworkOutput = true;
+	for(size_t i = 0; i < lastLayerOutput.size(); ++i) {
+		if(lastLayerOutput[i].size() != 1 && lastLayerOutput[i][0].size() != 1) {
+			validNetworkOutput = false;
+			break;
+		}
+	}
+
+	/* Construct the result of the network */
+	if(validNetworkOutput) {
+		for(size_t i = 0; i < lastLayerOutput.size(); ++i) {
+			outputMap.push_back(lastLayerOutput[i][0][0]);
+		}
+	} else {
+		throw InvalidResultArrayDimensionException;
+	}
 }
--- src/ConvNetwork.h
+++ src/ConvNetwork.h
@@ -1,17 +1,21 @@
 #pragma once
+
 #include <vector>
 #include <utility>
 
 #include "ConvLayer.h"
+#include "ImageBMP.h"
 
 class ConvNetwork
 {
 private:
     int numLayers;
     double learningSpeed;
-    std::vector <double> DesiredOutputMap;
-    std::vector <std::vector <double>> InputMap;
+    std::vector <double> desiredOutputMap;
+    std::vector <std::vector <double>> inputMap;
 	std::vector <ConvLayer> networkLayers;
+
+	ImageBMP inputImage;
 public:
     // Initialization of Network;
     ConvNetwork(const std::vector<int> neuronsInLayers, const std::vector<std::pair<int, int>> &convCoresDim);
@@ -20,7 +24,7 @@ public:
     ConvNetwork();
 
     // Processing an output vector;
-    void processOutput(std::vector <double> &outputMap);
+    void processInput(std::vector <double> &outputMap);
 
     // Getting InputMap from FILE;
     void getInput();
