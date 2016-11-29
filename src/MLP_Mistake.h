be4b1dae54f83ecc9a7233b1ea762a7ac2ff17f4
_VXD7 <youmukonpaku@excite.co.jp>
2016-11-29 23:47:45 +0300

    Deleted unnecessary files

--- src/MLP_Mistake.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vector>
-
-std::vector<std::vector<double>> MistakeWeights(double SpeedTrain, std::vector<double> Mistake, std::vector<double> BeforeOutput);
-std::vector<double> MistakeBias(double SpeedTrain, std::vector<double> Mistake);
-std::vector<double> SumMistake(std::vector<std::vector<double>> Weights, std::vector<double> Mistake);
