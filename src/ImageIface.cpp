a33f9031858480a2d1e9799ea25a7c7b215ecc18
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2016-12-01 00:09:12 +0300

    modified Image libs

--- src/ImageIface.cpp
+++ src/ImageIface.cpp
@@ -5,13 +5,20 @@
 #include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "C:\Users\Gleb\Desktop\stb_image.h"
 
 //https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-c
-void ImageIface::getImageList(std::vector<std::string> imageList) {
+void ImageIface::getImageList(std::vector<std::string> &imageList) {
 	for(size_t i = 0; i < imageList.size(); ++i) {
 		//if file exists
 		//else -- throw ex
+        std::ifstream map(imageList[i]);
+        if (map.good()) {
+            imgList.push_back(imageList[i]);
+        }
+        else
+            throw ; 
+        map.close();
 	}
 	
 }
@@ -27,9 +34,11 @@ void ImageIface::computeGrayscaleMatrix(std::string filename, std::vector <std::
 	if(w * h == 0) {
 		std::cout << "Error loading image! Probably image is not correct" << std::endl;
 	}
-
+    
 	makeImageMatrix(imageMatrix, map, w, h);
-
+    if (w < w_max || h < h_max) {
+        normalizeImageMatrix(imageMatrix);
+    }
 	stbi_image_free(map);
 }
 
@@ -59,4 +68,29 @@ void ImageIface::makeImageMatrix(std::vector<std::vector<double>> &matrix, unsig
 
 void ImageIface::computeGrayscaleMatrix(int number, std::vector <std::vector <double>> &imageMatrix) {
     computeGrayscaleMatrix(imgList[number], imageMatrix);
+}
+void ImageIface::normalizeEverything() {
+    h_max = w_max = 0;
+    for (int i = 0; i < imgList.size(); ++i) {
+        int w, h, comp;
+        unsigned char* map = stbi_load(imgList[i].c_str(), &w, &h, &comp, 1);
+        if (!map) {
+            //throw ex
+        }
+        if (w > w_max) { w_max = w; }
+        if (h > h_max) { h_max = h; }
+    }
+}
+void ImageIface::normalizeImageMatrix(std::vector<std::vector<double>> &imageMatrix) {
+    int h = imageMatrix.size();
+    int w = imageMatrix[0].size();
+    std::vector <double> zero(w_max);
+    for (int i = 0; i < h; ++i) {
+        for (int j = w; j < w_max - w; ++j) {
+            imageMatrix[i].push_back(0.0);
+        }
+    }
+    for (int i = h; i < h_max; ++i) {
+        imageMatrix.push_back(zero);
+    }
 }
--- src/ImageIface.h
+++ src/ImageIface.h
@@ -7,14 +7,15 @@
 
 class ImageIface {
 public:
-	void getImageList(std::vector<std::string> imageList);
+	void getImageList(std::vector<std::string> &imageList);
 	void normalizeEverything();
 
     void computeGrayscaleMatrix(std::string filename, std::vector <std::vector <double>> &imageMatrix);
     void computeGrayscaleMatrix(int number, std::vector <std::vector <double>> &imageMatrix);
 private:
+    int w_max, h_max;
 	std::vector<std::string> imgList;
 	void makeImageMatrix(std::vector<std::vector<double>> &matrix, unsigned char* map, int w, int h);
 
-	void normalizeImageMatrix(std::vector<std::vector<double>>, int w, int h);
+	void normalizeImageMatrix(std::vector<std::vector<double>> &imageMatrix);
 };
