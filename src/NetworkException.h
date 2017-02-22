c3307bc37d3971c66d9080dbc6abf2c4f290b06f
_VXD7 <youmukonpaku@excite.co.jp>
2017-02-22 13:45:11 +0300

    Added new exception type -- missing configuration

--- src/NetworkException.h
+++ src/NetworkException.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <exception>
+#include <vector>
 #include <string>
 
 //struct MyException : public std::exception {
@@ -18,3 +19,21 @@ public:
 	explicit InvalidResultArrayDimension(const std::string& message) : message_(message) {}
 	explicit InvalidResultArrayDimension() : message_("Invalid result array dimensions") {};
 }InvalidResultArrayDimensionException;
+
+struct MissingConfiguration : public std::exception {
+	std::string message_;
+	const char *what() const throw() {
+		return message_.c_str();
+	}
+
+public:
+	explicit MissingConfiguration(std::vector<std::string> missing) {
+		for(size_t i = 0; i < missing.size() - 1; ++i) {
+			message_ += missing[i] + ", ";
+		}
+
+		message_ += missing[missing.size()] + ".";
+	}
+
+	explicit MissingConfiguration() : message_("Invalid configuration") {};
+}MissingConfigurationException;
